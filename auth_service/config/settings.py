ea05071a5a599abe3eae25384d6d51e0100d2701
AhmedZhi10 <ahmed_shaban1000@icloud.com>
2025-10-15 16:54:11 +0300

    docker

--- auth_service/config/settings.py
+++ auth_service/config/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 INSTALLED_APPS = [
